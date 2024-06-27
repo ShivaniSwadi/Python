8430ae7c7247a5b25162644146be9c9381166769
Shivani Swadi <shivaniswadi@gmail.com>
2024-06-27 21:53:13 +0530

    changed the file name

---
+++ if_statement_examples.py
@@ -0,0 +1,14 @@
+x=60
+if x< 50:
+    print('x is less than 50')
+print('end of the program')
+
+if x> 50: print('x not less than 50')  #short hand
+
+x = 10   #nested if example
+if x < 50:
+    if x == 10:
+        print('x is equal to 10')
+    print('x is less than 50')
+print('End of the program')
+
--- less_than_50.py
+++
@@ -1,3 +0,0 @@
-x=10
-if x< 50:
-    print('x is less than 50')
